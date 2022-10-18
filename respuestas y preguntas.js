c38ba6e9a69accea212e9df8c7109caac6d21b98
germanUNAP <ccus.german.chura@gmail.com>
2022-10-19 10:25:32 +1300

    se agregan las respuestas al test

---
+++ respuestas y preguntas.js
@@ -0,0 +1,37 @@
+/*
+Responde las siguientes preguntas en la sección de comentarios:
+¿Qué es una variable y para qué sirve?
+una variable es un espacio de memoria que se designa para poder manejar, ya sea para guara nombres o quiza realizar operaciones matematicas
+¿Cuál es la diferencia entre declarar e inicializar una variable?
+Cuando se declara solo se manifioesta que se guarde un espacio en la memoria para la variable; mientras que cuando se inicializa, se le da el valor que se guardará en ese espaciod e memoria. Cabe realcar que si solo se inicializa el valor de la variable por defecto es "undefined"
+¿Cuál es la diferencia entre sumar números y concatenar strings?
+Cuando se suman enteros el resultado es un numero,;cuando se concatenan string el resultado es una cadena y apesar de que se concatene un valor numerico, el valor sequira siendo un string
+¿Cuál operador me permite sumar o concatenar?
+el operador de adicion "+"
+2️⃣ Determina el nombre y tipo de dato para almacenar en variables la siguiente información:
+Nombre = string
+Apellido = string
+Nombre de usuario en Platzi = string
+Edad = number
+Correo electrónico = string
+Mayor de edad = boolean
+Dinero ahorrado = float
+Deudas = float
+3️⃣ Traduce a código JavaScript las variables del ejemplo anterior y deja tu código en los comentarios.
+var nombre = "German"
+var apellido = "Chura"
+var platziUser = "Platziuser"
+var edad = 18
+var correo = "correo@pplatzi.com"
+var mayorEdad = true
+var dineroAhorrado = 1231.60
+var deudas = 4512.20
+4️⃣ Calcula e imprime las siguientes variables a partir de las variables del ejemplo anterior:
+Nombre completo (nombre y apellido)
+console.log(nombre + " " apellido);
+Dinero real (dinero ahorrado menos deudas)
+console.log("Mi dienero ahorrado es: "(dineroAhorrado - deudas));
+*/ 
+/*
+
+*/
